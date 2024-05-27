8af17053e7c71e8f799c796b0386466c15168f77
venkateshsakiri <venkateshsakiri377@gmail.com>
2024-05-27 23:34:02 +0530

    latest changes

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -2,16 +2,60 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AjaxLoaderComponent } from './ajax-loader/ajax-loader.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
     declarations: [
      AjaxLoaderComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatCardModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSnackBarModule,
+        MatProgressSpinnerModule,
+        MatGridListModule,
+        MatSidenavModule,
+        MatListModule,
+        OverlayModule
     ],
     exports: [
-        AjaxLoaderComponent
+        AjaxLoaderComponent,
+        MatToolbarModule,
+        MatButtonModule,
+        MatCardModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSnackBarModule,
+        MatProgressSpinnerModule,
+        MatGridListModule,
+        MatSidenavModule,
+        MatListModule,
+        OverlayModule
     ]
 })
 export class SharedModule { }
